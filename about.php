05a931775c0445bcd59590a6b265c6d065d5c146
Egor Opryshko <egorax1le@yandex.ru>
2018-03-29 22:08:30 +0300

    first task

---
+++ about.php
@@ -0,0 +1,53 @@
+<?php
+
+$name = ' Егор';
+$age = '22';
+$email = 'egorax1le@yandex.ru';
+$city = 'Видное';
+$about = 'Веб-программист';
+
+?>
+
+<html>
+    <head>
+        <title>Егор - Программист</title>
+		<meta charset="utf-8">
+		<style>
+			body {
+                font-family: sans-serif;
+            }
+            
+            dl {
+                display: table-row;
+            }
+            
+            dt, dd {
+                display: table-cell;
+                padding: 6px;
+            }
+		</style>
+	</head>
+	<body>
+		<h1>Страница студента<?= $name ?></h1>
+        <dl>
+            <dt>Имя</dt>
+            <dd><?= $name ?></dd>
+        </dl>
+        <dl>
+            <dt>Возраст</dt>
+            <dd><?= $age ?></dd>
+        </dl>
+        <dl>
+            <dt>Адрес электронной почты</dt>
+            <dd><a href=""><?= $email ?></a></dd>
+        </dl>
+        <dl>
+            <dt>Город</dt>
+            <dd><?= $city ?></dd>
+        </dl>
+        <dl>
+            <dt>О себе</dt>
+            <dd><?= $about ?></dd>
+        </dl>
+	</body>
+</html>
