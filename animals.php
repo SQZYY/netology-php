c4bec35f6381fd33be4513fd3f8d5cbfd5135433
Egor Opryshko <egorax1le@yandex.ru>
2018-04-06 00:54:09 +0300

    third task

---
+++ animals.php
@@ -0,0 +1,26 @@
+<?php
+
+ $animals = ['Panthera leo', 'Hippotigris', 'Tigris', 'Lupus', 'Castor canadensis', 'Grizzly',
+    'Myrmecophaga tridactyla', 'Dasyprocta', 'Phascolarctos cinereus', 'Notoryctes',
+    'Arctocephalus gazella', 'Procellariidae'];
+
+$firstName = [];
+$secondName = [];
+
+    foreach ($animals as $animal) {
+        $nameOfAnimal = str_word_count($animal, 1);
+            if (count($nameOfAnimal) > 1)
+            {
+                $firstName[] = $nameOfAnimal[0];
+                $secondName[] = $nameOfAnimal[1];
+            }
+    }
+
+shuffle($firstName);
+shuffle($secondName);
+
+for ($i = 0; $i <= count($firstName); $i++)
+{
+    echo $firstName[$i] . ' ' . $secondName[$i];
+    echo '<br>';
+}
