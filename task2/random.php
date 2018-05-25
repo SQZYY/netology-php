1cabf0575167530d5f319f9985959515c1fad2b9
Egor Opryshko <egorax1le@yandex.ru>
2018-05-25 17:20:31 +0300

    netology

--- task2/random.php
+++ task2/random.php
@@ -1,17 +1,16 @@
 <?php
 
 $x = rand(0,100);
-echo $x;
 
 $first = 1;
 $second = 1;
 
 while ($first >= $second) {
     if ($first > $x) {
-        echo ' - Задуманное число НЕ входит в числовой ряд';
+        echo "$x - задуманное число НЕ входит в числовой ряд";
         break;
     } elseif ($first == $x) {
-        echo ' - Задуманное число входит в числовой ряд';
+        echo "$x - задуманное число входит в числовой ряд";
         break;
     } else {
         $third = $first;
