ae91eea242228419851a5b2e3f13b191aefd0b23
Egor Opryshko <egorax1le@yandex.ru>
2018-05-31 14:25:58 +0300

    netology

--- task3/animals.php
+++ task3/animals.php
@@ -1,26 +1,26 @@
 <?php
 
- $animals = [
-     'Africa' => ['Panthera leo', 'Hippotigris'],
-     'Asia' => ['Tigris', 'Lupus'],
-     'South America' => ['Castor canadensis', 'Grizzly'],
-     'North America' => ['Myrmecophaga tridactyla', 'Dasyprocta'],
-     'Australia' => ['Phascolarctos cinereus', 'Notoryctes'],
-     'Antarctic' => ['Arctocephalus gazella', 'Procellariidae']
- ];
+$animals = [
+    'Africa' => ['Panthera leo', 'Hippotigris'],
+    'Asia' => ['Tigris', 'Lupus'],
+    'South America' => ['Castor canadensis', 'Grizzly'],
+    'North America' => ['Myrmecophaga tridactyla', 'Dasyprocta'],
+    'Australia' => ['Phascolarctos cinereus', 'Notoryctes'],
+    'Antarctic' => ['Arctocephalus gazella', 'Procellariidae']
+];
 
 $firstName = [];
 $secondName = [];
 
-    foreach ($animals as $continents => $animal) {
-        foreach ($animal as $name) {
-            $nameOfAnimal = str_word_count($name, 1);
-            if (count($nameOfAnimal) == 2) {
-                $firstName[] = $nameOfAnimal[0];
-                $secondName[] = $nameOfAnimal[1];
-            }
+foreach ($animals as $continents => $animal) {
+    foreach ($animal as $name) {
+        $nameOfAnimal = str_word_count($name, 1);
+        if (count($nameOfAnimal) == 2) {
+            $firstName[] = $nameOfAnimal[0];
+            $secondName[] = $nameOfAnimal[1];
         }
     }
+}
 
 shuffle($firstName);
 shuffle($secondName);
---
+++ task3/animals2.php
@@ -0,0 +1,32 @@
+<?php
+
+$animals = [
+    'Africa' => ['Panthera leo', 'Dermochelys coriacea'],
+    'Asia' => ['Phelsuma guentheri', 'Lupus'],
+    'South America' => ['Castor canadensis', 'Leptonycteris nivalis'],
+    'North America' => ['Myrmecophaga tridactyla', 'Alouatta macconnelli'],
+    'Australia' => ['Phascolarctos cinereus', 'Notoryctes'],
+    'Antarctic' => ['Arctocephalus gazella', 'Procellariidae']
+];
+
+$firstName = [];
+$secondName = [];
+
+foreach ($animals as $continents => $animal) {
+    echo '<h2>' . $continents . '</h2>';
+    foreach ($animal as $name) {
+        $nameOfAnimal = str_word_count($name, 1);
+        if (count($nameOfAnimal) == 2) {
+            $firstName[] = $nameOfAnimal[0];
+            $secondName[] = $nameOfAnimal[1];
+        }
+    }
+
+    shuffle($firstName);
+    shuffle($secondName);
+
+    for ($i = 0; $i < count($firstName); $i++)
+    {
+        echo $firstName[$i] . ' ' . $secondName[$i] . ', ';
+    }
+}
