f9a0d9144f8d1d757110e0dedf643c935a1a379c
Egor Opryshko <egorax1le@yandex.ru>
2018-06-04 22:50:31 +0300

    netology

--- task6/test.php
+++ task6/test.php
@@ -1,5 +1,10 @@
 <?php
 
+if (!isset($_GET['test'])) {
+    header('Location: ./list.php');
+    die();
+}
+
 $filename = './tests/' . $_GET['test'];
 
 if (file_exists($filename)) {
@@ -9,11 +14,17 @@ if (file_exists($filename)) {
     die();
 }
 
+foreach ($test as $key => $value) {
+    $answerNum = $value['result'];
+
+    var_dump($_POST["answer_[$key][$answerNum]"]);
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    if ($_POST['answer'] === $_POST['test']) {
+    if ("answer_[$key][$answerNum]" === "???") {
         echo 'Правильно!';
     } else {
         echo 'Неправильно!';
+    }
     }
 }
 
@@ -29,15 +40,17 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             <fieldset>
                 <?php
                 foreach ($test as $key => $value) {
+                    $answerNum = $value['result'];
                 ?>
-                <label for="test"><?=  $value['question']; ?></label><br>
+                <label for="answer_<?= $key; ?>"><?= $value['question']; ?></label><br>
                 <?php
                 foreach ($value['variables'] as $var) {
                     echo $var . '<br>';
                 }
                 ?>
-                <input type="hidden" name="test" value="<?= $value['result'] ?>">
-                <label><input type="text" name="answer"></label><br>
+                <label>
+                    <input type="text" name="answer_<?= $key; ?>">
+                </label><br>
                 <?php
                 }
                 ?>
